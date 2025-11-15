5f77d799f2448c6b95d8b0928b29c7aac174b846
thuongtruong109 <thuongtruongofficial@gmail.com>
2025-11-15 16:38:51 +0700

    fix(vm): full screen window setting modal

--- vm/index.py
+++ vm/index.py
@@ -10,74 +10,7 @@ import pyperclip
 
 from watcher import ScreenWatcher
 
-watchers = [
-    ScreenWatcher("./templates/cancel_capture.png"),
-    ScreenWatcher("./templates/install_software.png"),
-    ScreenWatcher("./templates/install_goless_vi.png"),
-    ScreenWatcher("./templates/skip_location_vi.png", threshold=0.75),
-    ScreenWatcher("./templates/skip_location_us.png", threshold=0.75),
-    # ScreenWatcher("./sample.png", threshold=0.75),
-]
-
-for w in watchers:
-    w.start()
-
-# template_path = "./templates/install_software.png"
-# threshold = 0.8
-# check_interval = 0.5
-
-# template = cv2.imread(template_path, cv2.IMREAD_UNCHANGED)
-# template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-# w, h = template_gray.shape[::-1]
-
-# found_event = threading.Event()
-# already_clicked = False
-
-# def watch_screen():
-#     global already_clicked
-#     while True:
-#         screenshot = pyautogui.screenshot()
-#         screenshot_np = np.array(screenshot)
-#         screenshot_gray = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
-
-#         res = cv2.matchTemplate(screenshot_gray, template_gray, cv2.TM_CCOEFF_NORMED)
-#         loc = np.where(res >= threshold)
-
-#         if loc[0].size > 0:
-#             found_event.set()
-#             if not already_clicked:
-#                 pt = (loc[1][0], loc[0][0])
-#                 x, y = pt[0] + w//2, pt[1] + h//2
-#                 pyautogui.click(x, y)
-#                 print(f"[Watcher] Clicked {template_path} at: {x}, {y}")
-#                 already_clicked = True
-#         else:
-#             found_event.clear()
-#             already_clicked = False
-
-#         time.sleep(check_interval)
-
-# watcher_thread = threading.Thread(target=watch_screen, daemon=True)
-# watcher_thread.start()
-
-# #############################################################
-command = 'dir D:\\*.iso /s /b'
-default_iso = r"D:\Soft\Windows_10_21H2_x64_Tiny.iso"
-result = subprocess.run(command, shell=True, capture_output=True, text=True)
-iso = result.stdout.strip() or default_iso
 DELAY = 0.5
-# #############################################################
-
-# VM
-if len(sys.argv) < 4:
-    print("⚠️ Usage: python index.py <name> <sock> <address>")
-    sys.exit(1)
-
-name = sys.argv[1]
-sock = sys.argv[2]
-address = sys.argv[3]
-
-host, port, user, passwd = (sock.split(":") + [""] * 4)[:4]
 
 def delay(sec=DELAY):
     time.sleep(sec)
@@ -97,149 +30,6 @@ def press_key(key, sec=DELAY):
 def hotkey(*keys, sec=DELAY):
     pyautogui.hotkey(*keys)
     delay(sec)
-
-# hotkey('win', 'd')
-
-# hotkey('win', 's')
-# type_text("virtual")
-# delay()
-# press_key("enter", 3)
-# press_key("enter")
-# delay(1)
-# hotkey('win', 'up')
-# delay(1)
-# hotkey('ctrl', 'n')
-
-# # Name and Operating System
-# move_click(737, 212)
-# type_text(name)
-
-# move_click(800, 259)
-# hotkey('ctrl', 'a')
-# press_key("backspace")
-# type_text(iso)
-
-# # Harware
-# move_click(715, 457)
-# move_click(1360, 317)
-# press_key("backspace")
-# type_text("4")
-
-# # Preset
-# move_click(726, 422)
-# move_click(672, 312)
-# move_click(1139, 370)
-# press_key("a")
-# hotkey('ctrl', 'enter')
-
-# # Network
-# move_click(732, 483)
-# move_click(1008, 399, clicks=2)
-# move_click(1358, 399, clicks=2)
-# move_click(675, 489)
-# move_click(812, 540)
-# type_text(host)
-# move_click(1075, 538)
-# hotkey('ctrl', 'a')
-# press_key("backspace")
-# type_text(port)
-# move_click(1258, 537)
-# type_text("8.8.8.8")
-# move_click(836, 562)
-# type_text(user)
-# move_click(1057, 563)
-# type_text(passwd)
-# move_click(777, 622)
-# type_text(address)
-# press_key("down")
-# press_key("enter")
-# move_click(676, 714)
-# move_click(676, 767)
-# move_click(912, 688)
-
-# # AntiOS
-# move_click(707, 801)
-# move_click(676, 427)
-# move_click(676, 606)
-
-# # Fingerprint
-# move_click(697, 830)
-# move_click(902, 433)
-
-# for _ in range(random.randint(1, 7)):
-#     press_key('down', sec=0.2)
-#     hotkey('ctrl', 'enter', sec=0.2)
-
-# press_key('enter')
-
-# # Settings
-# move_click(1270, 878)
-# hotkey('ctrl', 's')
-# move_click(833, 382)
-# move_click(900, 469)
-# type_text("bi")
-# hotkey('ctrl', 'enter')
-
-# move_click(600, 427)
-# move_click(873, 605)
-# move_click(610, 466)
-# move_click(1131, 485)
-# press_key('enter')
-
-# def wait_for_new_window(existing_windows, timeout=10):
-#     end_time = time.time() + timeout
-#     while time.time() < end_time:
-#         current_windows = gw.getAllWindows()
-#         for w in current_windows:
-#             if w.title not in existing_windows and w.title.strip() != "":
-#                 w.activate()
-#                 delay(0.5)
-#                 return w
-#         delay(0.5)
-#     raise Exception("No new window appeared")
-
-
-# existing_windows = set(w.title for w in gw.getAllWindows())
-
-# # Start
-# pyautogui.rightClick(30, 1010, duration=DELAY)
-# delay()
-# move_click(75, 753)
-# move_click(320, 753)
-
-# # Mount ISO
-# modal_window = wait_for_new_window(existing_windows)
-
-# hotkey('alt', 'tab')
-# delay(8)
-# hotkey('alt', 'tab')
-# move_click(1100, 1060, clicks=2)
-# delay(2)
-# pyautogui.moveTo(1079, 500, duration=0.5)
-# move_click(1079, 500, clicks=5)
-# type_text(iso, sec=0.1)
-
-# search_location = pyautogui.locateCenterOnScreen('mount_iso.png', confidence=0.8)
-
-# if search_location:
-#     pyautogui.moveTo(search_location, duration=0.3)
-#     pyautogui.click()
-#     print("Clicked mount iso button")
-# else:
-#     print("Image not found on screen.")
-
-# for _ in range(10):
-#     press_key('enter', sec=0.1)
-
-# delay(50)
-# move_click(1079, 500)
-# move_click(1090, 530)
-# delay(240)
-# move_click(1100, 1060, clicks=2)
-# delay(2)
-
-# In the VM window
-move_click(550, 300)
 
 def paste_into_vm(x, y, text):
     pyperclip.copy(text)
@@ -303,16 +93,229 @@ def search_vm(text):
     time.sleep(0.1)
     keyboard_vm("ctrl+v")
 
-# Turn on virtual keyboard
+def skip_chrome_location_callback(x, y):
+    pyautogui.click(x, y)
+    pyautogui.click(x, y)
+    print("Found chrome welcome: ", x, y)
+    delay(1)
+    move_click(341, 940)
+    move_click(470, 744)
+    hotkey('ctrl', 'w')
+    print("Clicked skip chrome welcome")
+
+watchers = [
+    ScreenWatcher("./templates/cancel_capture.png", min_delay=180),
+    ScreenWatcher("./templates/install_software.png"),
+    ScreenWatcher("./templates/install_goless_vi.png"),
+    ScreenWatcher("./templates/skip_location_vi.png", threshold=0.75),
+    ScreenWatcher("./templates/skip_location_us.png", threshold=0.75),
+    # ScreenWatcher("./sample.png", threshold=0.75),
+    ScreenWatcher("./templates/skip_chrome_welcome1.png", threshold=0.6, callback=skip_chrome_location_callback),
+    ScreenWatcher("./templates/skip_chrome_welcome2.png", threshold=0.6, callback=skip_chrome_location_callback)
+]
+
+for w in watchers:
+    w.start()
+
+# #############################################################
+command = 'dir D:\\*.iso /s /b'
+default_iso = r"D:\Soft\Windows_10_21H2_x64_Tiny.iso"
+result = subprocess.run(command, shell=True, capture_output=True, text=True)
+iso = result.stdout.strip() or default_iso
+# #############################################################
+if len(sys.argv) < 4:
+    print("⚠️ Usage: python index.py <name> <sock> <address>")
+    sys.exit(1)
+
+name = sys.argv[1]
+sock = sys.argv[2]
+address = sys.argv[3]
+host, port, user, passwd = (sock.split(":") + [""] * 4)[:4]
+# ##############################################################
+
+hotkey('win', 'd')
+
+hotkey('win', 's')
+type_text("virtual")
+delay(1)
+press_key("enter", 4)
+press_key("enter")
+delay(1)
+hotkey('win', 'up')
+delay(1)
+hotkey('ctrl', 'n')
+delay(1)
+
+# Name and Operating System
+move_click(737, 212)
+delay()
+type_text(name)
+
+move_click(800, 259)
+hotkey('ctrl', 'a')
+press_key("backspace")
+type_text(iso)
+
+# Harware
+move_click(715, 457)
+move_click(1360, 317)
+press_key("backspace")
+type_text("4")
+
+# Preset
+move_click(726, 422)
+move_click(672, 312)
+move_click(1139, 370)
+press_key("a")
+hotkey('ctrl', 'enter')
+
+# Network
+move_click(732, 483)
+move_click(1008, 399, clicks=2)
+move_click(1358, 399, clicks=2)
+move_click(675, 489)
+move_click(812, 540)
+type_text(host)
+move_click(1075, 538)
+hotkey('ctrl', 'a')
+press_key("backspace")
+type_text(port)
+move_click(1258, 537)
+type_text("8.8.8.8")
+move_click(836, 562)
+type_text(user)
+move_click(1057, 563)
+type_text(passwd)
+move_click(777, 622)
+type_text(address)
+press_key("down")
+press_key("enter")
+move_click(676, 714)
+move_click(676, 767)
+move_click(912, 688)
+
+# AntiOS
+move_click(707, 801)
+move_click(676, 427)
+move_click(676, 606)
+
+# Fingerprint
+move_click(697, 830)
+move_click(902, 433)
+
+for _ in range(random.randint(1, 7)):
+    press_key('down', sec=0.2)
+    hotkey('ctrl', 'enter', sec=0.2)
+
+delay(1)
+press_key('enter')
+
+# Settings
+move_click(1270, 878)
+hotkey('ctrl', 's')
+move_click(833, 382)
+move_click(900, 469)
+type_text("bi")
+hotkey('ctrl', 'enter')
+
+move_click(600, 427)
+move_click(873, 605)
+move_click(610, 466)
+move_click(1131, 485)
+press_key('enter')
+
+def wait_for_new_window(existing_windows, timeout=10):
+    end_time = time.time() + timeout
+    while time.time() < end_time:
+        current_windows = gw.getAllWindows()
+        for w in current_windows:
+            if w.title not in existing_windows and w.title.strip() != "":
+                w.activate()
+                delay(0.5)
+                return w
+        delay(0.5)
+    raise Exception("No new window appeared")
+
+
+existing_windows = set(w.title for w in gw.getAllWindows())
+
+# Start
+pyautogui.rightClick(30, 1010, duration=DELAY)
+delay()
+move_click(75, 753)
+move_click(320, 753)
+
+# Mount ISO
+modal_window = wait_for_new_window(existing_windows)
+
+hotkey('alt', 'tab')
+delay(8)
+hotkey('alt', 'tab')
+move_click(1100, 1060, clicks=2)
+delay(2)
+pyautogui.moveTo(1079, 500, duration=0.5)
+move_click(1079, 500, clicks=5)
+type_text(iso, sec=0.1)
+
+search_location = pyautogui.locateCenterOnScreen('templates/mount_iso.png', confidence=0.8)
+
+if search_location:
+    pyautogui.moveTo(search_location, duration=0.3)
+    pyautogui.click()
+    print("Clicked mount iso button")
+else:
+    print("Image not found on screen.")
+
+for _ in range(10):
+    press_key('enter', sec=0.1)
+
+delay(50)
+move_click(1079, 500)
+move_click(1090, 530)
+delay(240)
+move_click(1100, 1060, clicks=2)
+delay(2)
+
+# Open fullsize VM window
+move_click(1857, 89)
+
+# In the VM window
+move_click(550, 300)
+
+# Open settings
 move_click(30, 1003)
 move_click(23, 903)
 delay(1)
-move_click(800, 531)
-pyautogui.moveTo(150, 600, duration=0.5)
-pyautogui.scroll(-600)
-move_click(152, 573)
-move_click(414, 334)
+
+setting_location = pyautogui.locateCenterOnScreen('templates/window_settings.png', confidence=0.75)
+
+if setting_location:
+    x, y = setting_location
+    pyautogui.moveTo(x, y - 60, duration=0.3)
+    pyautogui.click()
+    pyautogui.click()
+    print("Clicked window setting full size modal")
+else:
+    print("Image not found on screen.")
+
+# Turn on virtual keyboard
+move_click(1220, 394)
+move_click(30, 843)
+move_click(365, 300)
 delay(1)
+
+keyboard_location = pyautogui.locateCenterOnScreen('templates/keyboard.png', confidence=0.75)
+
+if keyboard_location:
+    pyautogui.moveTo(keyboard_location, duration=0.3)
+    pyautogui.click()
+    pyautogui.click()
+    pyautogui.mouseDown()
+    pyautogui.moveTo(306, 562, duration=0.7)
+    pyautogui.mouseUp()
+    print("Clicked keyboard modal")
+else:
+    print("Image not found on screen.")
 
 # Open Chrome
 paste_into_vm(110, 1000, "chrome")
--- vm/watcher.py
+++ vm/watcher.py
@@ -1,28 +1,27 @@
-# watcher.py
+import pyautogui
 import cv2
 import numpy as np
 import threading
-import pyautogui
 import time
 
 class ScreenWatcher:
-    def __init__(self, template_path, threshold=0.8, check_interval=0.5):
+    def __init__(self, template_path, threshold=0.8, check_interval=0.5, min_delay=0, callback=None):
         self.template_path = template_path
         self.threshold = threshold
         self.check_interval = check_interval
+        self.min_delay = min_delay
+        self.callback = callback
 
-        # Load template
         self.template = cv2.imread(template_path, cv2.IMREAD_UNCHANGED)
         self.template_gray = cv2.cvtColor(self.template, cv2.COLOR_BGR2GRAY)
         self.w, self.h = self.template_gray.shape[::-1]
 
-        # Thread control
-        self.found_event = threading.Event()
+        self.detect_start_time = None
         self.already_clicked = False
+
         self.thread = threading.Thread(target=self._watch_screen, daemon=True)
 
     def start(self):
-        """Start watching thread"""
         self.thread.start()
 
     def _watch_screen(self):
@@ -35,16 +34,26 @@ class ScreenWatcher:
             loc = np.where(res >= self.threshold)
 
             if loc[0].size > 0:
-                self.found_event.set()
+                if self.detect_start_time is None:
+                    self.detect_start_time = time.time()
 
-                if not self.already_clicked:
-                    pt = (loc[1][0], loc[0][0])
-                    x, y = pt[0] + self.w // 2, pt[1] + self.h // 2
-                    pyautogui.click(x, y)
-                    print(f"[ScreenWatcher] Clicked {self.template_path} at: {x}, {y}")
-                    self.already_clicked = True
+                elapsed = time.time() - self.detect_start_time
+
+                if elapsed >= self.min_delay:
+                    if not self.already_clicked:
+                        x = loc[1][0] + self.w // 2
+                        y = loc[0][0] + self.h // 2
+
+                        if self.callback:
+                            pyautogui.moveTo(x, y)
+                            self.callback(x, y)
+                        else:
+                            pyautogui.click(x, y)
+                            print(f"[ScreenWatcher] Clicked {self.template_path} after {self.min_delay}s delay")
+
+                        self.already_clicked = True
             else:
-                self.found_event.clear()
+                self.detect_start_time = None
                 self.already_clicked = False
 
             time.sleep(self.check_interval)
